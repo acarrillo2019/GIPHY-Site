baceaeb78fe43e0c2f38ba7475ee69c04823720b
acarrillo2019 <alric21@aol.com>
2020-01-08 15:07:05 -0700

    Added my own API key

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@ var topics = ["birds", "dogs", "carrots", "bugs", "cars", "boats", "batman"];
 var gifNum = 10;
 
 
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=HbeuwKqfIIBRYZdteUvBd2Nwwp4GFQr4";
 
 $.ajax({
     url: queryURL,
